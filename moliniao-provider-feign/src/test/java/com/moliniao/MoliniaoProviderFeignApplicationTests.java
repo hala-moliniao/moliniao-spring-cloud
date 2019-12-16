63cc9961ece01f1330168e54411b12c7e20fbfb1
YCKJ2786 <limingzhou@cloudwalk.cn>
2019-12-16 20:55:54 +0800

    添加文件

---
+++ moliniao-provider-feign/src/test/java/com/moliniao/MoliniaoProviderFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moliniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoliniaoProviderFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
