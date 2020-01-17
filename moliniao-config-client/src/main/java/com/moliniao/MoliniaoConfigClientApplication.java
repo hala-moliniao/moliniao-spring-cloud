1d795a63f893c91fee0e084e6af3b0e86ee4a5f0
YCKJ2786 <limingzhou@cloudwalk.cn>
2020-01-17 19:06:49 +0800

    新增配置文件

---
+++ moliniao-config-client/src/main/java/com/moliniao/MoliniaoConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.moliniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoliniaoConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoliniaoConfigClientApplication.class, args);
+    }
+
+}
